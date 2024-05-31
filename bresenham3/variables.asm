1c19c8f98d102354fed59c4d95c2a0bd74b70581
Andrew Dansby <andydansby@gmail.com>
2024-05-31 06:29:49 -0400

    Update variables.asm

--- bresenham3/variables.asm
+++ bresenham3/variables.asm
@@ -1,48 +1,47 @@
 ;org $8080
 
-line_x1:	defw 0000	;$8080	line start point X
-line_y1:	defw 0015	;$8082	line start point Y
-line_x2:	defw 0255 	;$8084	line end point X
-line_y2:	defw 0165	;$8086	line end point Y
+line_x1:	defw 0000	;	line start point X
+line_y1:	defw 0015	;	line start point Y
+line_x2:	defw 0255 	;	line end point X
+line_y2:	defw 0165	;	line end point Y
 
-PUBLIC dx
-	dx:			defw 0000;$8088
-PUBLIC dy
-	dy:			defw 0000;$808A
+
 PUBLIC dxABS
-	dxABS:      defw 0000;$808C
+	dxABS:      defw 0000
 PUBLIC dyABS
-	dyABS:      defw 0000;$808E
+	dyABS:      defw 0000;
 
-PUBLIC fraction			;$8094
+PUBLIC fraction			;
 fraction:	defw 0000
 
-x1:	defw 0000			;$8094
-x2:	defw 0000			;$8096
-y1:	defw 0000			;$8098
-y2:	defw 0000			;$809A
-
-PUBLIC plot_x			;$809C
+PUBLIC plot_x			;
 plot_x:		defb 00
 
-PUBLIC plot_y			;$809D
+PUBLIC plot_y			;
 plot_y:		defb 00
 
+stepy:		defb 00		;
+stepx:		defb 00		;
 
-;fraction:	defw 0000
-stepy:		defb 00		;$80A2
-stepx:		defb 00		;$80A3
-
-PUBLIC gfx_x			;$9264
+PUBLIC gfx_x			;
 gfx_x:		defw 0000
 
-PUBLIC gfx_y			;$9266
+PUBLIC gfx_y			;
 gfx_y:		defw 0000
 
 
-PUBLIC X_PositionBits	;$80A4
+PUBLIC X_PositionBits	;
 X_PositionBits:
 defb 128,64,32,16,8,4,2,1
+
+;not used
+;dx:
+;dy:
+
+;x1:	defw 0000
+;x2:	defw 0000
+;y1:	defw 0000
+;y2:	defw 0000
 
 
 end_of_everything:
