19eff4db1472882d20befdc5a47ace397e4e4c60
Andrew Dansby <andydansby@gmail.com>
2024-06-05 22:57:40 -0400

    Add files via upload

---
+++ bresenham5/deltaY.asm
@@ -0,0 +1,29 @@
+deltaY_case:			;08118H
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;error1 += deltaY;
+; error1 = error1 + deltaY
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
+	ld HL, (error1)
+	ld DE, (deltaY)
+	add HL, DE
+	ld (error1), HL
+;<-------------------------------------------------
+; error1 is the answer found in HL
+;<-------------------------------------------------
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;x += stepx;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	ld A, (stepX)
+	ld HL, (line_x1)
+	add A, L
+	ld L, A
+	ld (line_x1), HL
+;;answer in HL and pass to variable
+
+
+	jp check_deltaX
+
+	;OK
